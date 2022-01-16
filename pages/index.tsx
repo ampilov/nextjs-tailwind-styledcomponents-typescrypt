77b37747c874ae3a2befd89054ff459f75866303
Anton Ampilov <anton_ampilov@mail.ru>
2022-01-16 19:41:43 +0300

    add description

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,19 @@ import tw from "twin.macro"
 export default function Home() {
   return (
     <Wrapper>
-      hello
+      <Heading_1> This project configured for using Next.js + tailwindCSS + styled-components + TS </Heading_1>
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`
+h-[100vh]
 flex
-bg-blue-400
+justify-center
+bg-indigo-200
+`
+const Heading_1 = tw.h1`
+text-3xl
+font-bold
+self-center
 `
